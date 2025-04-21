4c22439d7418a32c41600a454b0138f85625ca6f
Prallav Aggarwal <prallavagg002@gmail.com>
2025-04-22 01:14:34 +0530

    Admin dashboard and task list completed.

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -8,27 +8,70 @@ const TaskList = () => {
                     <h3 className="bg-red-400 px-3 py-1 rounded text-sm">
                         High
                     </h3>
-                    <h4>Date here</h4>
+                    <h4 className="text-sm">Date here</h4>
                 </div>
-                <h2>Make a tea</h2>
+                <h2 className="mt-5 text-2xl font-semibold">Make a tea</h2>
+                <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid saepe earum dolores impedit, delectus quae?</p>
             </div>
-            <div className="h-full w-[40%] rounded-2xl bg-purple-500 flex-shrink-0 ">
-
+            <div className="h-full w-[40%] rounded-2xl bg-blue-500 flex-shrink-0 p-5 ">
+            <div className="flex justify-between items-center">
+                    <h3 className="bg-red-400 px-3 py-1 rounded text-sm">
+                        High
+                    </h3>
+                    <h4 className="text-sm">Date here</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-semibold">Make a tea</h2>
+                <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid saepe earum dolores impedit, delectus quae?</p>
             </div>
-            <div className="h-full w-[40%] rounded-2xl bg-purple-500 flex-shrink-0 ">
-
+            <div className="h-full w-[40%] rounded-2xl bg-yellow-500 flex-shrink-0 p-5">
+            <div className="flex justify-between items-center">
+                    <h3 className="bg-red-400 px-3 py-1 rounded text-sm">
+                        High
+                    </h3>
+                    <h4 className="text-sm">Date here</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-semibold">Make a tea</h2>
+                <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid saepe earum dolores impedit, delectus quae?</p>
             </div>
-            <div className="h-full w-[40%] rounded-2xl bg-purple-500 flex-shrink-0 ">
-
+            <div className="h-full w-[40%] rounded-2xl bg-green-500 flex-shrink-0 p-5">
+            <div className="flex justify-between items-center">
+                    <h3 className="bg-red-400 px-3 py-1 rounded text-sm">
+                        High
+                    </h3>
+                    <h4 className="text-sm">Date here</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-semibold">Make a tea</h2>
+                <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid saepe earum dolores impedit, delectus quae?</p>
             </div>
-            <div className="h-full w-[40%] rounded-2xl bg-purple-500 flex-shrink-0 ">
-                
+            <div className="h-full w-[40%] rounded-2xl bg-red-500 flex-shrink-0 p-5">
+            <div className="flex justify-between items-center">
+                    <h3 className="bg-red-400 px-3 py-1 rounded text-sm">
+                        High
+                    </h3>
+                    <h4 className="text-sm">Date here</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-semibold">Make a tea</h2>
+                <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid saepe earum dolores impedit, delectus quae?</p>
             </div>
-            <div className="h-full w-[40%] rounded-2xl bg-purple-500 flex-shrink-0 ">
-                
+            <div className="h-full w-[40%] rounded-2xl bg-orange-500 flex-shrink-0 p-5">
+            <div className="flex justify-between items-center">
+                    <h3 className="bg-red-400 px-3 py-1 rounded text-sm">
+                        High
+                    </h3>
+                    <h4 className="text-sm">Date here</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-semibold">Make a tea</h2>
+                <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid saepe earum dolores impedit, delectus quae?</p>
             </div>
-            <div className="h-full w-[40%] rounded-2xl bg-purple-500 flex-shrink-0 ">
-                
+            <div className="h-full w-[40%] rounded-2xl bg-purple-500 flex-shrink-0 p-5">
+            <div className="flex justify-between items-center">
+                    <h3 className="bg-red-400 px-3 py-1 rounded text-sm">
+                        High
+                    </h3>
+                    <h4 className="text-sm">Date here</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-semibold">Make a tea</h2>
+                <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid saepe earum dolores impedit, delectus quae?</p>
             </div>
         </div>
     )
